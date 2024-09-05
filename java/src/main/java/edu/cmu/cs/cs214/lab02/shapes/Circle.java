b086413f180d85605641b5a2fbe437c507282b7f
UMSiyuanLou <lousiyuan2004@gmail.com>
2024-09-05 16:47:23 -0400

    improve encapsulation for java

--- java/src/main/java/edu/cmu/cs/cs214/lab02/shapes/Circle.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/shapes/Circle.java
@@ -1,9 +1,17 @@
 package edu.cmu.cs.cs214.lab02.shapes;
 
 public class Circle implements Shape {
-    public double radius;
+    private double radius;
 
     public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
         this.radius = radius;
     }
 
